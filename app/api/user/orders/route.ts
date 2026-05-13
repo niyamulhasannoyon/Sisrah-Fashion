8f058413cf37643859fd0806e56cb10fec3cbc1e
MD. Niyamul Hasan <niyamulhasan@MDs-MacBook-Air.local>
2026-05-13 15:36:54 +0600

    upate nodejs

--- app/api/user/orders/route.ts
+++ app/api/user/orders/route.ts
@@ -5,6 +5,8 @@ import dbConnect from '@/lib/dbConnect';
 import Order from '@/models/Order';
 import User from '@/models/User';
 
+export const dynamic = 'force-dynamic';
+
 export async function GET(req: Request) {
   try {
     await dbConnect();
