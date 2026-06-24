750dea57ea335a20f9ef1ebccb858986d19a24ac
MD. Niyamul Hasan <niyamulhasan@MDs-MacBook-Air.local>
2026-06-24 20:37:42 +0600

    ”hi”

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -18,7 +18,7 @@ export function Footer() {
           <div className="flex flex-col gap-4">
             <Link href="/" className="text-xl font-black tracking-tight text-white">
               {settings?.logo ? (
-                <img src={getDirectImageLink(settings.logo)} alt="AS SIDRAT" className="h-16 md:h-22 w-auto object-contain brightness-0 invert" />
+                <img src={getDirectImageLink(settings.logo)} alt="AS SIDRAT" className="h-12 md:h-16 w-auto object-contain brightness-0 invert" />
               ) : (
                 "AS SIDRAT"
               )}
--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -60,7 +60,7 @@ export function Navbar() {
         <div className="mx-auto flex max-w-7xl items-center justify-between px-6 py-2 md:py-3 sm:px-8 lg:px-12">
         <Link href="/" className="flex items-center">
           {settings?.logo ? (
-            <img src={getDirectImageLink(settings.logo)} alt="AS SIDRAT" className="h-16 md:h-24 w-auto object-contain" />
+            <img src={getDirectImageLink(settings.logo)} alt="AS SIDRAT" className="h-8 md:h-12 w-auto object-contain" />
           ) : (
             <span className="text-xl font-bold tracking-tight text-loomra-black">AS SIDRAT</span>
           )}
